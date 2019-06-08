e712d572da1e66b9472f6e146578643b8d5a1fb5
Andreas Neuenschwander <uparix@users.noreply.github.com>
2019-06-08 16:17:53 +0200

    alert window asyn call example

--- lib/imports.c
+++ lib/imports.c
@@ -6,6 +6,8 @@ int main() {
 
     // Call JS function (eval)
     emscripten_run_script("console.log('Hello from C!')");
+    
+    emscripten_async_run_script("alert('Hello from C - ASYNC!')", 2000);
 
     return 1;
 }
